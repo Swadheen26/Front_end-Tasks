1a8e206da6f3bdc172e8bcf7052a266d48bba75d
Swadheen Prusty <Swadheenrinku@gmail.com>
2024-05-15 13:55:55 +0530

    problem6 added

---
+++ JS/Fun with Array - I/Square_numbers-HOF.js
@@ -0,0 +1,8 @@
+let arr = [1, 2, 3, 4];
+
+// Use map to create an array with the square of each number
+let squaredNumbers = arr.map(el => el ** 2);
+
+// Print the squared numbers
+squaredNumbers.forEach(el => console.log(el));
+
